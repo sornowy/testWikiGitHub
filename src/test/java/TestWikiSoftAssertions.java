e5fe9fe4a37e47fb0d9481f4bde187d6cb0cad19
Timur <sornowy@gmail.com>
2024-04-29 02:02:32 +0700

    add shouldHave

--- src/test/java/TestWikiSoftAssertions.java
+++ src/test/java/TestWikiSoftAssertions.java
@@ -2,17 +2,25 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Configuration.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class TestWikiSoftAssertions {
     @Test
 
     void wikiPages(){
+        browserSize = "1920x1080";
         open("https://github.com/selenide/selenide");
 
         $("#repository-container-header nav").$$("li").get(5).click();
-        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
-        $(".")
-        sleep(10000);
+        //$("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();//Реализация через поиск
+        $(".js-wiki-sidebar-toggle-display ul").$$("li").last().$("button").click();
+        //$$(".js-wiki-sidebar-toggle-display ul").$("li").$("a").click();
+        $(byText("SoftAssertions")).click();
+        $("#wiki-body").shouldHave(text("3. Using JUnit5 extend test class:"));
+        holdBrowserOpen = true;
+
     }
 }
