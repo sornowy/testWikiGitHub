d98439b7bad5f9398e703cd40e16aa0330523580
Timur <sornowy@gmail.com>
2024-04-28 23:11:46 +0700

    Find SoftAssertions

--- src/test/java/TestWikiSoftAssertions.java
+++ src/test/java/TestWikiSoftAssertions.java
@@ -1,9 +1,18 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class TestWikiSoftAssertions {
     @Test
 
     void wikiPages(){
+        open("https://github.com/selenide/selenide");
 
+        $("#repository-container-header nav").$$("li").get(5).click();
+        $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+        $(".")
+        sleep(10000);
     }
 }
